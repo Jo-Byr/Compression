288f72e57fe24735ab5acc5f86e208455cdc902d
Jonathan Bouyer <jonathanbouyer24@gmail.com>
2023-11-30 15:01:51 +0100

    Clean code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Matrice de quantification JPEG
 Z = np.array(
     [
         [16, 11, 10, 16, 24,  40,  51,  61],
@@ -16,6 +16,7 @@ Z = np.array(
     ]
 )
 
+# Matrice de quantification alternative permettant un meilleur taux de compression
 Z2 = np.array(
     [[16,  12,  12,  18,  26,  40,  50,  56],
      [11,  13,  19,  23,  31,  56,  61,  50],
@@ -27,26 +28,25 @@ Z2 = np.array(
      [75,  95,  94, 103, 115, 102, 102,  96]],
 )
 
-encode_diff = True
-
 
 def RMSE(I0, I1):
+    """ Mesure de la RMSE entre les images I0 et I1 """
     assert I0.shape == I1.shape
     H, W = I0.shape
-    return np.sqrt(np.sum((I1 - I0)**2) / (H * W))
+    return np.sqrt(np.sum((I1.astype(np.float32) - I0.astype(np.float32))**2) / (H * W))
 
 
 def zigzag(H, W):
-    """ Retourne une liste de tuples représentant les coordonnées du zigzag d'encodage """
+    """ Retourne une liste de tuples représentant les coordonnées du zigzag d'encodage d'ume image HxW """
     x = 0
     y = 0
-    L = [(x,y)]
+    L = [(x, y)]
 
     b = True
 
     while x < W and y < H:
         if x == 0 or y == 0 or x == W - 1 or y == H - 1:
-            if b and y==0 or not b and y == H-1:
+            if b and y == 0 or not b and y == H-1:
                 x += 1
             else:
                 y += 1
@@ -64,7 +64,7 @@ def zigzag(H, W):
 
 
 def plage(L):
-    """ Retourne le codage en plage de la liste """
+    """ Retourne le codage en plage de la liste L """
     i = 1
     n = 0
     res = []
@@ -80,7 +80,7 @@ def plage(L):
 
 
 def un_plage(L):
-    """ Retourne le décodage en plage de la liste """
+    """ Retourne le décodage en plage de la liste L """
     res = []
     i = 1
     for n in L:
@@ -90,13 +90,14 @@ def un_plage(L):
 
 
 def bits(i):
+    """ Retourne le nombre de bits nécessaire pour encoder l'entier i """
     return int(np.log(i) / np.log(2)) + 1
 
 
 def pos_encoding(L):
-    """ Prend la liste des positions non nulles [1, 1, 0, 0, 1, 0, 0, 0...], et le nombre de bits permettant
-     l'encodage le plus efficace """
-    max_bits = bits(max(L))
+    """ Prend la liste des positions non nulles [1, 1, 0, 0, 1, 0, 0, 0...] encodée en plage et retourne le nombre
+     de bits optimal pour encoder cette liste """
+    max_bits = bits(max(L))  # Nombre de bits pour encoder le nombre maximal de la liste sur un bloc
     min_len_bits = None
     min_len = None
     for b in range(1, max_bits + 1):
@@ -115,6 +116,10 @@ def pos_encoding(L):
 
 
 def calc_size(non_zero_val_matrix, non_zero_pos_matrix):
+    """ Retourne le nombre de bits nécessaire pour encoder l'image compressée
+    non_zero_val_matrix est une liste de liste comportant les coefficients non nuls de chaque bloc
+    non_zero_pos_matrix est une liste de liste comportant les positions des coefficients non nuls de chaque bloc,
+    encodée en plage """
     non_zero_val_bits = []
     for non_zero_val_list in non_zero_val_matrix.flatten():
         if non_zero_val_list:
@@ -144,86 +149,111 @@ def image_reconstruction(DCT_q):
     return I_rec
 
 
-def DCT_reconstruction(val, L):
-    H, W = val.shape  # val et L sont de taille (H/8, W/8) avec H,W la taille de l'image de base
+def DCT_reconstruction(non_zero_val_matrix, non_zero_pos_matrix):
+    """ Reconstruit une matrice de l'ensemble des DCT quantifiée 8x8 à partir de :
+    non_zero_val_matrix : liste de liste comportant les coefficients non nuls de chaque bloc
+    non_zero_pos_matrix : liste de liste comportant les positions des coefficients non nuls de chaque bloc, encodée
+    en plage
+    """
+    H, W = non_zero_val_matrix.shape  # val et L sont de taille (H/8, W/8) avec H,W la taille de l'image de base
     H *= 8
     W *= 8
     DCT = np.zeros((H, W))
     zz_coords = zigzag(8, 8)
 
+    last_top_left_val = 0  # Valeur du dernier coin sup. gauche (pour codage prédictif)
+
     for i in range(int(H/8)):
         for j in range(int(W/8)):
-            l = un_plage(L[i, j])
-            v = val[i, j]
-            assert len(np.where(np.array(l) == 1)[0]) == len(v)
-            n = 0
-            top_left = True
+            block_non_zero_pos = un_plage(non_zero_pos_matrix[i, j])
+            block_non_zero_values = non_zero_val_matrix[i, j]
+
+            assert len(np.where(np.array(block_non_zero_pos) == 1)[0]) == len(block_non_zero_values)
+            n = 0  # Nombre de valeurs non-nulles traitées
+            top_left = True  # Booléen indiquant si la valeur en train d'être traitée est un coin sup. gauche de bloc
             for k, coord in enumerate(zz_coords):
                 if encode_diff:
-                    value = 0
-                    if l[k]:
-                        value = v[n]
-                        if top_left:
-                            if not(i == 0 and j == 0):
-                                value += last_top_left_val
-                            last_top_left_val = value
-                        n += 1
-                    elif k == 0:
+                    # Avec codage prédictif
+                    value = 0  # Valeur du pixel
+                    if top_left:
+                        # Les coins sup. gauche sont codés comme la différence de leur valeur à celle du coin sup.
+                        # gauche précédent
                         value = last_top_left_val
+
+                    if block_non_zero_pos[k]:
+                        # Si la valeur à cette position est non-nulle :
+                        value += block_non_zero_values[n]  # mise à jour de la valeur
+                        n += 1  # Incrément du compteur de valeurs non-nulles traitées
+
+                    if top_left:
+                        # Mise à jour de la dernière valeur du coin sup. gauche
+                        last_top_left_val = value
+
+                    # Mise à jour de la matrice
                     DCT[coord[1] + 8*i, coord[0] + 8*j] = value
                     top_left = False
                 else:
-                    if l[k]:
-                        DCT[coord[1] + 8 * i, coord[0] + 8 * j] = v[n]
+                    # Sans codage prédictif
+                    if block_non_zero_pos[k]:
+                        DCT[coord[1] + 8 * i, coord[0] + 8 * j] = block_non_zero_values[n]
                         n += 1
     return DCT
 
 
 def compression(I):
+    """ Retourne la version compressée de I et le taux de compression associé """
     global Z
     H, W = I.shape
     If = I.astype(np.float32)
 
-    non_zero_val_matrix = []  # Matrice H/8 x W/8 contenant les listes de valeurs qtifiees non nulles
+    non_zero_val_matrix = []  # Matrice H/8 x W/8 contenant les listes de valeurs quantifiées non nulles
     non_zero_pos_matrix = []  # Matrice H/8 x W/8 contenant les listes de 0 et 1 indiquant les positions des valeurs non nulles
     zz_coords = zigzag(8, 8)
+    last_top_left_val = 0  # Valeur du coin sup. gauche précédent (pour codage prédictif)
 
     for i in range(0, H, 8):
         line_non_zero_val = []
         line_non_zero_pos = []
         for j in range(0, W, 8):
-            dct = cv2.dct(If[i:i+8, j:j+8])
-            T = np.fix(dct/Z)
+            dct = cv2.dct(If[i:i+8, j:j+8])  # DCT d'un bloc 8x8 de l'image
+            T = np.fix(dct/Z)  # Quantification
 
             block_non_zero_pos = []
             block_non_zero_val = []
             for coord in zz_coords:
+                # Parcours en zigzag
                 x = coord[0]
                 y = coord[1]
                 value = T[y, x]
+
                 if encode_diff:
+                    # Si encodage prédictif : calcul de la différence au coin sup. gauche précédent pour les coins
+                    # sup. gauches
                     if x == y == 0:
-                        if not(i == j == 0):
-                            value -= last_top_left_val
+                        value -= last_top_left_val
                         last_top_left_val = T[y, x]
 
-                non_zero = int(value != 0)
+                non_zero = int(value != 0)  # 0 si valeur nulle, 0 sinon
                 block_non_zero_pos.append(non_zero)
 
                 if non_zero:
+                    # Récupération de la valeur si elle n'est pas nulle
                     block_non_zero_val.append(value)
 
-            line_non_zero_pos.append(plage(block_non_zero_pos))
+            line_non_zero_pos.append(plage(block_non_zero_pos))  # Codage en plage des positions
             line_non_zero_val.append(block_non_zero_val)
 
         non_zero_pos_matrix.append(line_non_zero_pos)
         non_zero_val_matrix.append(line_non_zero_val)
+
     non_zero_pos_matrix = np.array(non_zero_pos_matrix, dtype=object)
     non_zero_val_matrix = np.array(non_zero_val_matrix, dtype=object)
 
+    # Calcul du taux de compression
     size = calc_size(non_zero_val_matrix.flatten(), non_zero_pos_matrix.flatten())
     taux_comp = H * W * 8 / size
 
+    # Reconstruction de la matrice de blocs de DCT
     DCT_rec = DCT_reconstruction(non_zero_val_matrix, non_zero_pos_matrix)
 
     return image_reconstruction(DCT_rec), taux_comp
@@ -269,7 +299,6 @@ def main():
 
     plt.subplot(2, 2, 4)
     plt.hist(DCT.ravel(), bins=500, range=[-200, 200])
-    #plt.hist(DCTlog.ravel(), bins=500)
     plt.title('Histogramme de distribution des intensités')
 
     plt.show()
@@ -299,6 +328,7 @@ def main():
 
 
 def test_rand():
+    """ Fonction qui génère des variations aléatoires de Z pour chercher une version plus efficace """
     global Z
     I = cv2.imread("Images/04.png", 0)
     I_rec, taux_comp = compression(I)
@@ -325,6 +355,15 @@ def test_rand():
 
 
 if __name__ == "__main__":
-    z_fac = 2.14
+    """ z_fac pour atteindre 15 suivant le mode utilisé :
+    Pas d'encodage prédictif et Z de base : 3.06
+    Encodage prédictif et Z de base : 2.14
+    Pas d'encodage prédictif et Z2 : 2.79
+    Encodage prédictif et Z2 : 1.91 """
+    use_Z2 = True  # Utilisation de la matrice modifiée Z2 plutôt que de la version JPEG Z
+    encode_diff = True  # Utilisation de l'encodage prédictif
+    z_fac = 1.91  # Facteur de multiplication de la matrice Z
+    if use_Z2:
+        Z = Z2
     Z = np.round(Z.astype(np.float32) * z_fac).astype(np.uint16)
     main()
