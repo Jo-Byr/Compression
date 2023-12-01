b7c6fd676c104b98a3b0de4a0f93419c7d671eba
Jonathan Bouyer <jonathanbouyer24@gmail.com>
2023-12-01 07:11:21 +0100

    Add automatic reconstruction of slices

--- dwt.py
+++ dwt.py
@@ -11,6 +11,21 @@ def RMSE(I0, I1):
     return np.sqrt(np.sum((I1.astype(np.float32) - I0.astype(np.float32))**2) / (H * W))
 
 
+def reconstruct_coeff_slices(coeff_arr):
+    """ Reconstruit la liste de dictionnaire coeff_slices associée aux coefficients coeff_arr """
+    H, W = coeff_arr.shape
+    assert H == W  # On ne supporte que les images carrées
+    size0 = int(H / 2**level)
+    res = [(slice(None, size0, None), slice(None, size0, None))]
+    for i in range(1, level + 1):
+        size1 = int(H / 2**(level - i + 1))
+        size2 = int(H / 2**(level - i))
+        res.append({'ad': (slice(None, size1, None), slice(size1, size2, None)),
+                    'da': (slice(size1, size2, None), slice(None, size1, None)),
+                    'dd': (slice(size1, size2, None), slice(size1, size2, None))})
+    return res
+
+
 def main():
     I = cv2.imread("Images/04.png", 0)
     coeffs_DWT = pywt.wavedec2(I, onde, 'symmetric', level)
@@ -31,10 +46,10 @@ def main():
 
     r = 0.5  # A fixer pour minimiser la RMSE
 
-    coeff_arr_rec = coeff_arr_q.copy()
-    coeff_slices_rec = coeff_slices_q.copy()
-
     # Dequantification
+    coeff_arr_rec = coeff_arr_q.copy()
+    coeff_slices_rec = reconstruct_coeff_slices(coeff_arr_rec)
+
     Q = coeff_arr_rec[coeff_slices_rec[0]]  # Matrice de coefficient à déquantifier
     coeff_arr_rec[coeff_slices_rec[0]] = (Q + r * np.sign(Q)) * delta[0]
 
@@ -45,10 +60,12 @@ def main():
 
     # Reconstruction
     coeffs_rec = pywt.array_to_coeffs(coeff_arr_rec, coeff_slices_rec)
+
     # Mise au même format que coeffs_DWT
-    coeffs_rec = [coeffs_rec[0]] + [list(coeffs_rec[i].values()) for i in range(1, 4)]
+    coeffs_rec = [coeffs_rec[0]] + [list(coeffs_rec[i].values()) for i in range(1, level + 1)]
     for i in range(1, level + 1):
         coeffs_rec[i] = [coeffs_rec[i][1], coeffs_rec[i][0], coeffs_rec[i][2]]
+
     I_rec = pywt.waverec2(coeffs_rec, onde)
 
     diff = np.abs(I_rec.astype(np.int16) - I.astype(np.int16)).astype(np.uint8)
@@ -74,12 +91,11 @@ def main():
 
 if __name__ == "__main__":
     onde = "haar"
-    level = 3
+    level = 2
     delta = {
         0: 1,
         1: 2,
-        2: 2,
-        3: 2
+        2: 2
     }
     assert len(delta) == level + 1
     main()
